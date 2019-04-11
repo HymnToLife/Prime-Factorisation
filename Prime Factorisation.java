3ffbf1cee513784444c36131c6727abcf378cabe
qihua.wu <qihua.wu@tozmart.com>
2019-04-11 20:56:54 +0800

    First Version

---
+++ Prime Factorisation.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Program{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int num;
+        do {
+            while (!input.hasNextInt()) {
+                System.out.println("Please enter a positive integer");
+                System.exit(0);
+            }
+            num = input.nextInt();
+        } while (num < 1);
+
+        System.out.println(primeFactorisation(num));
+
+    }
+
+    public static String primeFactorisation(int n) {
+
+        int counter = 0;
+
+        if (n == 1) {
+
+            return "1";
+
+        }
+
+        String result = "" + n + " = ";
+
+        for (int i = 2; i <= n; i++) {
+
+            if (n%i == 0) {
+
+                result += (i + " x ");
+
+                n = n/i;
+                i = 1;
+
+                counter++;
+
+            }
+
+            if (n == 1) {
+
+                break;
+
+            }
+
+        }
+
+        result = result.substring(0, result.length() - 3);
+
+        if (counter < 2) {
+
+            result = result.substring(0, result.length() - 1 * (result.indexOf("=") + 2));
+            result += "is a prime number";
+
+        }
+
+        return result;
+
+    }
+}
